eda1b471b34ba7c3c101ece31b4d62a0d6caf812
jjuiddong <jjuiddong@gmail.com>
2018-10-17 00:05:56 +0900

    update etc

---
+++ Src/WebCrawl/config.h
@@ -0,0 +1,28 @@
+//
+// 2018-10-16, jjuiddong
+// WebCrawling Configuration
+//
+// JSon Script
+//{
+   //"crawl-interval-seconds" : "100"
+   //, "name-rect" : "0, 0, 100, 100"
+   //, "stock-rect" : "0, 0, 100, 100"
+   //, "offset-y" : "200"
+   //, "next-site-interval-seconds" : "5"
+   //	
+   //, "sites" :
+   //[
+   //	{
+   //		"url" : "https://www.investing.com/equities/amazon-com-inc"
+   //		, "name" : "Amazon.com Inc (AMZN)"
+   //	}
+
+   //	,{
+   //		"url" : "https://www.investing.com/equities/netflix,-inc."
+   //		, "name" : "Netflix Inc (NFLX)"
+   //	}
+
+   //]
+//}
+#pragma once
+
